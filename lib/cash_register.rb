aab8a2ffa74be5658f48045ccc160b3a3b2d8f34
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-22 17:24:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ def apply_discount
   if discount != 0
     self.total = (total * ((100.0 - discount.to_f)/100)).to_i
   end
-  puts "After the discount, the total comes to $#{self.total}."
+  print "After the discount, the total comes to $#{self.total}."
 end
 
 def items
